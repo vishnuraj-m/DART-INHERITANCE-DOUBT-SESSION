885022caa6da0383e3d86300dbf52526a53559ff
vishnuacappella <acappella.studiov@gmail.com>
2022-08-02 03:51:41 +0530

    Abstrct

--- bin/inheritance.dart
+++ bin/inheritance.dart
@@ -1,25 +1,30 @@
-class Animal {
-  void sayHello() {
-    print('Animal Say Hello');
-  }
+abstract class Animal {
+  void sayHello();
 }
 
-class Human extends Animal {
-  void sayName() {
-    print('Human Say Name');
-  }
-
+class Human implements Animal {
   @override
   void sayHello() {
-    print('Say Hello Human');
-    super.sayHello();
+    print('Hello Human');
   }
 }
+
+// class Human extends Animal {
+//   void sayName() {
+//     print('Human Say Name');
+//   }
+
+//   @override
+//   void sayHello() {
+//     print('Say Hello Human');
+//     super.sayHello();
+//   }
+// }
 
 void main(List<String> arguments) {
   // *obj human
 
   final human = Human();
   human.sayHello();
-  human.sayName();
+  // human.sayName();
 }
